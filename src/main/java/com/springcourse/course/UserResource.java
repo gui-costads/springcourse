ee847c1a0632ee13a63ed8bf5ca9fcc2b0507d55
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-03-21 22:05:59 -0300

    add user entity and resource

---
+++ src/main/java/com/springcourse/course/UserResource.java
@@ -0,0 +1,19 @@
+package com.springcourse.course;
+
+import com.springcourse.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "maria", "maria@gmail.com", "1235685", "123");
+        return  ResponseEntity.ok().body(user);        
+    }
+
+
+}
