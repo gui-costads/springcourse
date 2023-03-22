fc6b108bfc892835111657eb5fac8da8be4a1042
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-03-21 22:50:23 -0300

    configuration h2 database and application properties

--- src/main/java/com/springcourse/course/UserResource.java
+++ src/main/java/com/springcourse/course/UserResource.java
@@ -12,7 +12,7 @@ public class UserResource {
     @GetMapping
     public ResponseEntity<User> findAll(){
         User user = new User(1L, "maria", "maria@gmail.com", "1235685", "123");
-        return  ResponseEntity.ok().body(user);        
+        return  ResponseEntity.ok().body(user);
     }
 
 
